8e5ee25138ac0c73fa764ca0b1ed1b8490b53164
andytudhope <tuddy0525@gmail.com>
2023-03-22 09:23:56 +0200

    form validation

--- packages/dapp/src/components/Forgive.js
+++ packages/dapp/src/components/Forgive.js
@@ -17,6 +17,7 @@ function Forgive () {
   const [forgiven, setForgiven] = useState('')
   const [amount, setAmount] = useState('')
   const [showScanner, setShowScanner] = useState(false)
+  const [validForm, setValidForm] = useState(false)
   // eslint-disable-next-line
   const [error, setError] = useState(null)
 
@@ -29,6 +30,38 @@ function Forgive () {
   const handleError = (err) => {
     console.error(err)
   }
+
+  const isValidEthereumAddress = (address) => {
+    return /^(0x)?[0-9a-fA-F]{40}$/.test(address)
+  };
+
+  const handleInputChange = (e) => {
+    const input = e.target.value;
+    const name = e.target.name;
+  
+    if (name === 'forgiven') {
+      setForgiven(input);
+      if (!isValidEthereumAddress(input)) {
+        setError('Please enter a valid Ethereum address')
+        setValidForm(false)
+      } else {
+        setError('')
+      }
+    } else if (name === 'amount') {
+      const amountNum = Number(input);
+      if (isNaN(amountNum) || amountNum <= 0) {
+        setError('Please enter a valid amount greater than 0')
+        setValidForm(false)
+      } else {
+        setError('')
+      }
+      setAmount(input)
+
+      const isAmountValid = !isNaN(amount)
+      const isReceiverValid = isValidEthereumAddress(forgiven)
+      setValidForm(isAmountValid && isReceiverValid)
+    }
+  };
 
   async function handleForgive (e) {
     e.preventDefault()
@@ -75,7 +108,7 @@ function Forgive () {
             id='forgiven'
             name='forgiven'
             value={forgiven}
-            onChange={(e) => setForgiven(e.target.value)}
+            onChange={handleInputChange}
             className='w-full px-4 py-2 bg-white border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-500'
           />
           <button
@@ -101,17 +134,21 @@ function Forgive () {
           id='amount'
           name='amount'
           value={amount}
-          onChange={(e) => setAmount(e.target.value)}
+          onChange={handleInputChange}
           className='w-full px-4 py-2 bg-white border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-500'
         />
         <button
           type='submit'
-          className='w-1/2 px-4 py-2 mx-auto text-white bg-[#233447] rounded-md hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-indigo-500'
+          className={`w-1/2 px-4 py-2 mx-auto text-white rounded-md focus:outline-none focus:ring ${
+            validForm ? 'bg-[#233447] hover:bg-indigo-700 focus:ring-indigo-500' : 'bg-gray-300 cursor-not-allowed'
+          }`}
           onClick={handleForgive}
+          disabled={!validForm}
         >
           Forgive
         </button>
       </form>
+      {error && <p className='mt-4 text-red-500'>{error}</p>}
     </div>
   )
 }
--- packages/dapp/src/components/Propose.js
+++ packages/dapp/src/components/Propose.js
@@ -17,6 +17,7 @@ function Propose () {
   const [receiver, setReceiver] = useState('')
   const [amount, setAmount] = useState('')
   const [showScanner, setShowScanner] = useState(false)
+  const [validForm, setValidForm] = useState(false);
   // eslint-disable-next-line
   const [error, setError] = useState(null)
 
@@ -30,8 +31,41 @@ function Propose () {
     console.error(err)
   }
 
+  const isValidEthereumAddress = (address) => {
+    return /^(0x)?[0-9a-fA-F]{40}$/.test(address)
+  };
+
+  const handleInputChange = (e) => {
+    const input = e.target.value;
+    const name = e.target.name;
+  
+    if (name === 'receiver') {
+      setReceiver(input);
+      if (!isValidEthereumAddress(input)) {
+        setError('Please enter a valid Ethereum address')
+        setValidForm(false)
+      } else {
+        setError('')
+      }
+    } else if (name === 'amount') {
+      const amountNum = Number(input);
+      if (isNaN(amountNum) || amountNum <= 0) {
+        setError('Please enter a valid amount greater than 0')
+        setValidForm(false)
+      } else {
+        setError('')
+      }
+      setAmount(input)
+
+      const isAmountValid = !isNaN(amount)
+      const isReceiverValid = isValidEthereumAddress(receiver)
+      setValidForm(isAmountValid && isReceiverValid)
+    }
+  };
+
   async function handlePropose (e) {
     e.preventDefault()
+
     openLoading('Please sign this transaction')
 
     // Call the propose function with the inputted address and amount
@@ -73,7 +107,7 @@ function Propose () {
             id='receiver'
             name='receiver'
             value={receiver}
-            onChange={(e) => setReceiver(e.target.value)}
+            onChange={handleInputChange}
             className='w-full px-4 py-2 bg-white border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-500'
           />
           <button
@@ -99,17 +133,21 @@ function Propose () {
           id='amount'
           name='amount'
           value={amount}
-          onChange={(e) => setAmount(e.target.value)}
+          onChange={handleInputChange}
           className='w-full px-4 py-2 bg-white border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-500'
         />
         <button
           type='submit'
-          className='w-1/2 px-4 py-2 mx-auto text-white bg-[#233447] rounded-md hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-indigo-500'
+          className={`w-1/2 px-4 py-2 mx-auto text-white rounded-md focus:outline-none focus:ring ${
+            validForm ? 'bg-[#233447] hover:bg-indigo-700 focus:ring-indigo-500' : 'bg-gray-300 cursor-not-allowed'
+          }`}
           onClick={handlePropose}
+          disabled={!validForm}
         >
           Propose
         </button>
       </form>
+      {error && <p className='mt-4 text-red-500'>{error}</p>}
     </div>
   )
 }
